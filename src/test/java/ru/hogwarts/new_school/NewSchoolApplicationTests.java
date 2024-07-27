90112355041bba1b3a052e360aef059269dfbd51
Nikita Malinkin <nik.malina20@gmail.com>
2024-07-27 09:50:56 +0300

    Initial commit

---
+++ src/test/java/ru/hogwarts/new_school/NewSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.new_school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
